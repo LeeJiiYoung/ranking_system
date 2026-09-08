56db27332606b9953abf62d3631ad2f2230d87f5
jylee <jylee@sewonit.com>
2026-09-08 16:39:50 +0900

    init

--- backend/src/main/java/com/ranking/RankingApplication.java
+++ backend/src/main/java/com/ranking/RankingApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RankingApplication {
 
 	public static void main(String[] args) {
-
+		SpringApplication.run(RankingApplication.class, args);
 	}
 
 }
